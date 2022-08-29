40489bd34259f290d2b99bb52efd530144eff9d3
andrew-can-code <110874049+andrew-can-code@users.noreply.github.com>
2022-08-29 17:21:32 -0400

    init

---
+++ root/javascript/loader.js
@@ -0,0 +1,20 @@
+var loader = {
+  initialize : function () {
+    var html = '<div class="loading-overlay"></div>' +
+    '<div class="loading-overlay-image-container">' +
+      '<img src="../root/images/loading-image-v2.gif" class="loading-overlay-img" />' +
+    '</div>'; 
+          
+      $( 'body' ).append( html );
+  },
+
+      showLoader : function () {
+        $( '.loading-overlay').show();
+        $( '.loading-overlay-image-container' ).show();
+      },
+
+      hideLoader : function () {
+        $( '.loading-overlay' ).hide();
+        $( '.loading-overlay-image-container' ).hide();
+      }
+    }
